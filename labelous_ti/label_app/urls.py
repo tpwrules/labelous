e77ce3c4088bc4258d87c7c7576f78266481ad35
Thomas Watson <twatson52@icloud.com>
2020-02-14 18:11:12 -0600

    add logic to serve labelme junk and get labeler showing

--- labelous_ti/label_app/urls.py
+++ labelous_ti/label_app/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 
 from . import views
+from . import tool_static_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+
+    # the labeler needs a crapton of static files. we are allegedly clubbing
+    # baby seals by serving them with django, but it works for now.
+    path('', tool_static_views.tool),
+    path('annotationTools/css/<file>', tool_static_views.lm_static,
+        {"dir": "annotationTools/css"}),
+    path('annotationTools/js/<file>', tool_static_views.lm_static,
+        {"dir": "annotationTools/js"}),
+    path('Icons/<file>', tool_static_views.lm_static,
+        {"dir": "Icons"}),
+    path('Images/f/<file>', tool_static_views.lm_static,
+        {"dir": "Images/f"}),
+    path('Annotations/f/<file>', tool_static_views.lm_static,
+        {"dir": "Annotations/f"}),
 ]
