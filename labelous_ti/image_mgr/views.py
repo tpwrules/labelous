57d45479bdaeaf5a91c5400c152a270a846dccde
Thomas Watson <twatson52@icloud.com>
2020-02-23 15:54:06 -0600

    add basic authentication

--- labelous_ti/image_mgr/views.py
+++ labelous_ti/image_mgr/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 import shutil
 
 from . import models
 
 # serve images to the labeler
+@login_required
 def image_file(request, image_id):
     # regex only allows numbers through
     image_id = int(image_id)
--- labelous_ti/label_app/urls.py
+++ labelous_ti/label_app/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, re_path
 from . import views
 from . import tool_static_views
 import image_mgr.views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
 
     # the labeler needs a crapton of static files. we are allegedly clubbing
     # baby seals by serving them with django, but it works for now.
-    path('', tool_static_views.tool),
+    path('', login_required(tool_static_views.tool)),
     path('annotationTools/css/<file>', tool_static_views.lm_static,
         {"dir": "annotationTools/css"}),
     path('annotationTools/js/<file>', tool_static_views.lm_static,
@@ -18,8 +19,11 @@ urlpatterns = [
     re_path(r'^Images/f/img(?P<image_id>[0-9]+).jpg$',
         image_mgr.views.image_file),
     re_path(r'^Annotations/f/img(?P<image_id>[0-9]+).xml$',
-        views.get_annotation_xml),
-    path('annotationTools/perl/submit.cgi', views.post_annotation_xml),
-    path('annotationTools/perl/fetch_image.cgi', views.next_annotation),
-    path('annotationTools/perl/fetch_prev_image.cgi', views.prev_annotation),
+        login_required(views.get_annotation_xml)),
+    path('annotationTools/perl/submit.cgi',
+        login_required(views.post_annotation_xml)),
+    path('annotationTools/perl/fetch_image.cgi',
+        login_required(views.next_annotation)),
+    path('annotationTools/perl/fetch_prev_image.cgi',
+        login_required(views.prev_annotation)),
 ]
