34912736a6c4164cea3b6e11f842c97c6c0dff4a
Thomas Watson <twatson52@icloud.com>
2020-03-12 10:42:48 -0500

    implement function to start a new annotation

--- labelous_ti/browser/views.py
+++ labelous_ti/browser/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from django.core.exceptions import SuspiciousOperation
 from django.contrib import messages
+from django.db.models import OuterRef, Exists
 
+from datetime import datetime, timezone
+
+import image_mgr.models
 import label_app.models
 
 # show all the annotations the user has
 def annotation_list(request):
     if request.method == "POST":
-        return redirect(handle_modify(request))
+        return handle_modify(request)
 
     annotations = label_app.models.Annotation.objects.order_by('pk').filter(
         annotator=request.user, deleted=False)
@@ -26,22 +30,46 @@ def handle_modify(request):
     try:
         action = request.POST["action"]
         anno_id = request.POST["anno_id"]
-        if action not in ("delete_anno",):
+        if action not in ("delete_anno", "new_anno",):
             raise Exception("invalid action {}".format(action))
     except Exception as e:
         raise SuspiciousOperation("bad post") from e
 
-    if action != "annotate_new":
+    if action != "new_anno":
         try:
             anno_id = int(anno_id)
             annotation = label_app.models.Annotation.objects.get(pk=anno_id,
                 annotator=request.user, deleted=False, finished=False)
         except Exception as e:
             raise SuspiciousOperation("bad annotation") from e
+    else:
+        anno_id = None
 
     if action == "delete_anno":
         annotation.deleted = True
         annotation.save()
         messages.add_message(request, messages.SUCCESS, "Annotation deleted.")
+    elif action == "new_anno":
+        # get all the annotations the user is already working or has completed
+        existing_annos = label_app.models.Annotation.objects.filter(
+            annotator=request.user, deleted=False)
+        # then look for an image where an annotation for it doesn't exist
+        new_image = image_mgr.models.Image.objects.filter(
+            ~Exists(existing_annos.filter(image__pk=OuterRef("pk"))))[0:1]
+        try:
+            new_image = new_image.get()
+        except image_mgr.models.Image.DoesNotExist:
+            messages.add_message(request, messages.ERROR,
+                "There are no more images to annotate. Good job!")
+            return redirect("annos_in_progress")
+        
+        # if one exists, create an annotation for it
+        new_anno = label_app.models.Annotation(
+            annotator=request.user, image=new_image,
+            edit_key=b"", last_edit_time=datetime.now(timezone.utc))
+        new_anno.save()
 
-    return request.resolver_match.url_name
+        # then send the user off to edit it
+        return redirect(new_anno.edit_url)
+
+    return redirect(request.resolver_match.url_name)
