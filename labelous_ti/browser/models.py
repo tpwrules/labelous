a2a0f574fddf11ec7e6d34c911eb7685723f0a37
Thomas Watson <twatson52@icloud.com>
2020-03-19 10:41:11 -0500

    use email to log in instead of username

--- labelous_ti/browser/models.py
+++ labelous_ti/browser/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(unique=True)
+    REQUIRED_FIELDS = []
