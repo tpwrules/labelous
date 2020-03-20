16eb75a29c4e21e47ab9bcf9a45ecb28c793823b
Thomas Watson <twatson52@icloud.com>
2020-03-19 19:10:07 -0500

    fix being unable to view user admin panel

--- labelous_ti/browser/admin.py
+++ labelous_ti/browser/admin.py
@@ -4,5 +4,16 @@ from .models import User
 
 class CustomUserAdmin(UserAdmin):
     ordering = ('email',)
+    exclude = ('username',)
+
+    # stolen from UserAdmin
+    fieldsets = (
+        (None, {'fields': ('password',)}),
+        (('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',
+                'user_permissions'),
+        }),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}))
 
 admin.site.register(User, CustomUserAdmin)
